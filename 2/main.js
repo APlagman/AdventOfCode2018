e2470e584294555dadc80f63088cc60adfad4942
Alex Plagman <alexplagman@gmail.com>
2018-12-02 11:16:54 -0800

    Day 2

---
+++ 2/main.js
@@ -0,0 +1,317 @@
+const input = `uqcipadzntnheslgvjjozmkfyr
+uqcipadzwtnhexlzvxjobmkfkr
+cqcipadpwtnheslgyxjobmkfyr
+ubnipadzwtnheslgvxjobmkfyw
+uqcisadzwtnheslgvxjfbmkfor
+uqcisaezwtnheslgvxkobmkfyr
+uqcguadzwtnheslgvxjobmkfir
+uqcipadzmtnhesldvxdobmkfyr
+uqcipadzwtzheslgdxjtbmkfyr
+uquipadzwtcheslgvxjobmkfbr
+uqctpadzwtnhesjbvxjobmkfyr
+ueciparzwtnheslgvxjobmkfyx
+uqcipadzwtnhessgvxjkbmkfkr
+uqcipamzwtnheslgvxioamkfyr
+uciizadzwtnheslgvxjobmkfyr
+uqcieadzwtnhesfgvxeobmkfyr
+fqcipadzwtnreslgvkjobmkfyr
+uqcipadzrtnherlgvxjobmklyr
+uqcypadzwtnheslgvxjobmkxfr
+uqcipadzwtnhemlgvxjobmvfur
+uwciuadzwwnheslgvxjobmkfyr
+uqcipadzwtnhcscgvxjobmkuyr
+upripadzwtnheslovxjobmkfyr
+uqcipadzltnheslgvxjobmkftc
+uqcipadzwtnheslgvgjobmifsr
+uqoipadzwtnheslgvxjosmkfkr
+uqcipadzwtbhesrqvxjobmkfyr
+uqcipadzwtnheslpvxjobmhfyx
+uhcinadzwtnheslgvxjybmkfyr
+uqcipadzwtnhhslgvxjabmkbyr
+uecipadzwtnheslgvxjobqyfyr
+uqcipadfwtnheslwvxjobgkfyr
+uqcipadzvtnheshgvxzobmkfyr
+fqcipadzwtcheslgvxjobmkfyt
+uecipadzwtnheslgpxjbbmkfyr
+uqclpadzwtnheslgvnjobukfyr
+qqciprdzetnheslgvxjobmkfyr
+uqcipahpwtnheslgvxjtbmkfyr
+uqcidadzwtnhesljvxyobmkfyr
+uqciradswtnqeslgvxjobmkfyr
+uqcipadzwtrhmslgvxjobmkfyf
+urcipadzjtnheslgvxfobmkfyr
+uqcipadzwznheslgvxjobmkfcv
+uqcipadowtnheslgyxjobmkfym
+uqcigadzwtnheslgvxjoomkmyr
+uqjipafzwtnheslgvejobmkfyr
+uqcioadzwtnhhslgvxzobmkfyr
+uqcgpadkwtnheslgvxjobhkfyr
+ufciiadewtnheslgvxjobmkfyr
+uqoipadzwtnheslgvxjllmkfyr
+uqcipadzutnheslgwxxobmkfyr
+uqcipadzwtlheslgaxjobmkfwr
+uqcbpadzutnheslgvxjbbmkfyr
+uucipadzwvnhesngvxjobmkfyr
+uqcifadzwtnceslgvxjoumkfyr
+ujcipadzwteheslgvxjobmkfyj
+uqcipadzwtnheslqvxjobmkuyp
+uqcipadzwtnheslgvxjoxmkxyw
+uqcipaduwtnheslgvujmbmkfyr
+uicipadnwtnheslgvxjobmbfyr
+uqcipadzwteheslgvxjobbmfyr
+uqcipadzwgnneslgvxjobmklyr
+uqcipadzxtnhwslgvbjobmkfyr
+uqcipaxwwtnheslxvxjobmkfyr
+uocipadzwtnheslgvxjobqdfyr
+uqciaauzwtnheslgtxjobmkfyr
+uncipagzwtnkeslgvxjobmkfyr
+uqcipadzwtnhehlgvxjohdkfyr
+uqcipadzwtnheslgvxjobmspyz
+uccipadzwtnhvsltvxjobmkfyr
+uacipagzwtnheslgvxjoqmkfyr
+tqcipaduwtnheslgvxjobmmfyr
+uqcipadzwtnheslgvxqebmifyr
+uecipadthtnheslgvxjobmkfyr
+uocipadzwtnhdslgvkjobmkfyr
+uqcipadtwtnheslgvxhobmufyr
+uqkipadzwtnleslgtxjobmkfyr
+uqcipadzjunheslgvxjobmnfyr
+ubcipadzwtvheslgvxjobmkfyf
+uqcipadzwpfheslgvxjsbmkfyr
+uocipadzwtndeslgvxjobmmfyr
+uqcipadzwtnheslgtxjobhkfyq
+uqcipadzwtrheslgvxjobmyfya
+uqcipadzwtvheslgvxjolgkfyr
+uqcipidzwtaheslgvxjobmkfxr
+uyzixadzwtnheslgvxjobmkfyr
+uqyihadzwtnhedlgvxjobmkfyr
+uqcipadzwtnhesltvejobqkfyr
+uqciptdzwtnheslgyxlobmkfyr
+uqcipzdzwtnhzslgvxjosmkfyr
+uqcipadzwtnbeslgexjobmkfvr
+uqcipadzwtnheslcwxjobmkkyr
+uqcapadzwcnheslgvxjolmkfyr
+uqcjpadzwtnhejlgvxjxbmkfyr
+uqcipadwwtxweslgvxjobmkfyr
+uqmipadzwtnhezlgvxjobmkyyr
+uqcipubzwtnpeslgvxjobmkfyr
+uecvpadzwtnheslgvxjocmkfyr
+uqcipadzwfnheslgvxjibmkdyr
+uqcipadzwtnheslgvxvfbykfyr
+uqcipadzwtnheslgvgjoimkfyt
+dqcqpaqzwtnheslgvxjobmkfyr
+uqcipbdzwtnheslgvxjobmkghr
+jqcipadzwtnheslgvxjgbmkzyr
+uqcipadzwtnheslgvxqkqmkfyr
+uqcipadzptnheslgvxjxbokfyr
+uucijadzwtwheslgvxjobmkfyr
+uccfpadzwtnheslgvxjobpkfyr
+uqcipadzwtnheslgvxjobakeyq
+uqcipadzwtnheolgvxqobjkfyr
+imiipadzwtnheslgvxjobmkfyr
+uqcehadzwtnheslgvxjobmkuyr
+uqcipadzztnheslgvxjorokfyr
+rqcixadzwtnheelgvxjobmkfyr
+uqcipadzwtzheslgvxjodmkfyi
+uqcipaezwtnwuslgvxjobmkfyr
+uqcipadzwtnheslggxjobjkfyq
+uqcipadzwkghesagvxjobmkfyr
+uqcypqdzwtnheslgvxjobakfyr
+iqcipadzwtnhezltvxjobmkfyr
+uxcimadzwtnheslgvxjobmxfyr
+uqcipaizwtvhwslgvxjobmkfyr
+uqcipafzwtnheslgvxjpbmkfym
+uqcipadzwinheslgvxlobmpfyr
+uqcupadzwtnheslkvxmobmkfyr
+uqcapadzwtnhesrgvxjobmkfsr
+urcipafzwtnheslgvxjobmkfur
+uqcipaczwtnheslgvbjobmknyr
+uqcizadzztgheslgvxjobmkfyr
+uqcipfdzwtnhesxgvxjobmkfyw
+uqcipbdzwtnhyslgvxjobmcfyr
+uqcipadzwanhezlgvxjobmkfwr
+uvcipadzwtnheslgvxjbkmkfyr
+uqcipajzwtnseslgvxjobmkfyq
+uqcipvdzwtnheslgvmlobmkfyr
+uqcipadzdgnheslgmxjobmkfyr
+uqcipddzwtnhestgvpjobmkfyr
+umcipadzwtdheslgvxjzbmkfyr
+uqciuwdzwtnheslgvxjobmkflr
+uqcipadzwtnheslgsxabbmkfyr
+uceipadzwtnheslgvxjobgkfyr
+mqcipadzwtnhesrgvxjobmjfyr
+aqcipadvwtnheslgvxjobmkryr
+uqsipadzwtnofslgvxjobmkfyr
+uqcixadzwtfheslgvxjzbmkfyr
+uqcipadnwfnheslgvxjohmkfyr
+uqcivadzwtnheslfvxjobmkfyz
+uqciprdzwtnheslgvxjobmkjir
+uqcipadhbtnheslgvxjoxmkfyr
+fqcipadzwtnhesfgvxjobmkfye
+uqoipqdzwtnheqlgvxjobmkfyr
+uqcipadzwtnhesltvxmobmkzyr
+uqcipadzwtnhebqgvsjobmkfyr
+uqcipadzwtnheslglxjobmfbyr
+gqcipadzwtgheslgvxjobwkfyr
+uqcipadzwtnheslgfxjzbmlfyr
+ujcnpadzwtnheslrvxjobmkfyr
+ujcivadzwtnheglgvxjobmkfyr
+uqcitadzwgnheslgvxjofmkfyr
+uqcipahzatnhmslgvxjobmkfyr
+uqzipaizwtnheslgvujobmkfyr
+uqcipadzltnheylgvnjobmkfyr
+uqcidadzwtnhwsljvxyobmkfyr
+uqcipadzwtihetlgvxjobhkfyr
+oqcipabzwtnheslgvfjobmkfyr
+uqcipadzwtnveslgvxjobzkfzr
+uqcipadzwtjheslgqxjobmlfyr
+uqcnpadzztnheslgvxjobmkoyr
+uqciuadzwonheslgvxjobmkfyz
+tqcipadzwtnheslgvxaobmqfyr
+uqcipadtwtnhqslgvxjobmkeyr
+uqcipadzwbnheslgvajobmsfyr
+ubcopadzwtnhgslgvxjobmkfyr
+uqcipydzwtwheslgvxjobakfyr
+cqbijadzwtnheslgvxjobmkfyr
+uscipadowtnheslgvxjobmkfcr
+uqcipadzwtgheslnvxjobskfyr
+uqcipzdzwtnzeslgkxjobmkfyr
+uqcipawzwtnhrslgbxjobmkfyr
+uqcipadzatchyslgvxjobmkfyr
+uqcipadzotnpeslgvxjobmjfyr
+uqcipagzwtnheslgvxjobmvfyt
+uqcipadzwhnheslgvxyobmkfyo
+uqcipadzwtnheslgmqjobmkfyc
+uqcupadzwgnheslgvcjobmkfyr
+uqcipabzwbnheslgvxjobmkwyr
+uqciiadzwtnheslgvxjobmkfmz
+uqkipauzwtnheslgvxjjbmkfyr
+uqcipidzetnheslgvxjobmkfyi
+uqcipadzwtnheslgqxjokmkfmr
+uqcipadzqtnhesllvxjobmkfyk
+uqccpadzwtnheslgmxsobmkfyr
+uqcipadzwteheslgvljfbmkfyr
+uqcipadxwinheslgaxjobmkfyr
+uqcipadzwtnheslhvxyobmkfjr
+aqcipadzwnnheslgvxjqbmkfyr
+uvcipadzwtnheszgvxjobmkfyg
+uqcipahzmtnheslgvxjobmkfir
+ukcipadzbtnheslgvxjobmkfyb
+uqcipadzwtnhemlgvqjobmkfpr
+uqcipadzwtnheslgvmeobmkfpr
+uqciphdrwtnheslgvxjobmkfyw
+uqcipadzwtnheslevxqobzkfyr
+uqcipadzwknzeslgvxnobmkfyr
+wqcipadzwjnheslgvxjobbkfyr
+uqcipadzwtdheslgvmjobmkjyr
+uqvipadzwtnhextgvxjobmkfyr
+uqhipadzwtnheslwvxjzbmkfyr
+uqcipadzwtnherlgsxjobmksyr
+uqcipadzwtnhesqgvxjotmvfyr
+udcipadzwtnhekwgvxjobmkfyr
+uqcjprdzwtnheslgvxjobmkfpr
+uqcipadzatnheclgvqjobmkfyr
+uqcbpadzctnheslqvxjobmkfyr
+uqcipadzqtnhesluvxjobrkfyr
+uqcipadzwtnhcslgvxjoomwfyr
+uqcppadzwxnheslgwxjobmkfyr
+uqcipadcwtnheslrvxjdbmkfyr
+ukcipadzwtnhhslgvxjobmkgyr
+uqckpadzwtnheslgvxjokmkiyr
+uqcspadzwtjheslgvxjobmkfjr
+uqcipadpwtnhsslgvxjobmkfyu
+uqcepadzwtnheilgvbjobmkfyr
+jqcipadiwtnheslgvxjobmkjyr
+uqcipadzrtnseslgqxjobmkfyr
+sqmipadzwtnhewlgvxjobmkfyr
+uqcieadzhtnheslgvgjobmkfyr
+uqcipadzwkwhewlgvxjobmkfyr
+uqcipadzwtzheslgvxjpbqkfyr
+uzcipadzjtnheslgvxjobmlfyr
+uqcipadzwtnheslnvxjobmkfee
+uqciyanzwtnheslgvxjoimkfyr
+uqcipadqwtnheswghxjobmkfyr
+uycipadzwtnheslovxjobmofyr
+uqcipadzwtnheslgvxcozmxfyr
+uqmipadzwtnxezlgvxjobmkfyr
+uqcipadzftnheslgvxjotmkffr
+aqcipaizwtnhesagvxjobmkfyr
+uqcipcdzwtnheslgoajobmkfyr
+uqcypadgwtnhesbgvxjobmkfyr
+uqcipcdzwtnheslgvxjebmkfyb
+uhcvpadzwtnheslgvxjobzkfyr
+uqcipadzwtnpesagvxmobmkfyr
+uqcipadzwtnidslgvxjobmkfor
+uqcipadkwtnhesigvxjzbmkfyr
+uqcypadlwtnheslsvxjobmkfyr
+qqcipadzwtnheswgvxjobmkoyr
+uqcipadzwtnheslgvxjhbmmcyr
+uqcipadzwtnhesogvxjormkfmr
+uqcipadzwtnhetcgvxgobmkfyr`;
+
+const tests = [`abcdef`,`bababc`,`abbcde`,`abcccd`,`aabcdd`,`abcdee`,`ababab`];
+
+function repeatedTwoAndOrThree(string) {
+    const seen = new Array(26);
+    for (let i = 0; i < 26; ++i)
+        seen[i] = 0;
+
+    string.split('')
+    .forEach(letter => {
+        const index = letter.charCodeAt(0) - 'a'.charCodeAt(0);
+        ++seen[index];
+    });
+
+    let two = 0;
+    let three = 0;
+    if (seen.some(freq => freq === 2))
+        two = 1;
+    if (seen.some(freq => freq === 3))
+        three = 1;
+    return [two, three];
+}
+
+tests.forEach(str => {
+    console.log(`Test \'${str}\': ${repeatedTwoAndOrThree(str)}`);
+});
+
+const strings = input.split('\n');
+
+const part1 = strings
+.map(str => repeatedTwoAndOrThree(str))
+.reduce((total, next) => {
+    total[0] += next[0];
+    total[1] += next[1];
+    return total;
+});
+console.log(`Part 1 Answer: ${part1[0] * part1[1]}`);
+
+const test2 = `abcde
+fghij
+klmno
+pqrst
+fguij
+axcye
+wvxyz`;
+
+function sameLettersFor1Apart(strings) {
+    for (let i = 0; i < strings.length; ++i) {
+        for (let j = 0; j < strings.length; ++j) {
+            if (j === i)
+                continue;
+            let diff = 0;
+            let sameLetters = '';
+            for (let letter = 0; letter < strings[i].length; ++letter) {
+                if (strings[i][letter] !== strings[j][letter])
+                    ++diff;
+                else
+                    sameLetters += strings[i][letter];
+            }
+            if (diff === 1)
+                return sameLetters;
+        }
+    }
+}
+
+console.log(`Test for part 2: ${sameLettersFor1Apart(test2.split('\n'))}`);
+console.log(`Part 2 Answer: ${sameLettersFor1Apart(strings)}`);
